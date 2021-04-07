bb3239cc24fd2d4152d47fa4ed01dcb9e2a2e59f
Joanantha Matheus <jmvg.comp@gmail.com>
2021-04-07 16:00:32 -0300

    Test java-project

---
+++ java-project/src/main/java/dev/jmvg/bigpromo/BigpromoApplication.java
@@ -0,0 +1,13 @@
+package dev.jmvg.bigpromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigpromoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigpromoApplication.class, args);
+    }
+
+}
---
+++ java-project/src/test/java/dev/jmvg/bigpromo/BigpromoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.bigpromo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigpromoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
