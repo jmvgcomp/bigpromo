3006af96c0f3c3c87f0e727501fcb91f8e8d72f9
Joanantha Matheus <jmvg.comp@gmail.com>
2021-06-21 22:24:47 -0300

    busca na tabela

--- java-project/src/main/java/dev/jmvg/bigpromo/web/repository/PromocaoRepository.java
+++ java-project/src/main/java/dev/jmvg/bigpromo/web/repository/PromocaoRepository.java
@@ -1,6 +1,8 @@
 package dev.jmvg.bigpromo.web.repository;
 
 import dev.jmvg.bigpromo.web.domain.Promocao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +10,11 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+    @Query("select p from Promocao p where p.titulo like %:search% or " +
+            "p.site like %:search% or " +
+            "p.categoria.nome like %:search%")
+    Page<Promocao> findByTituloOrSiteOrCategoria(@Param("search") String search, Pageable pageable);
 
     @Transactional(readOnly = false)
     @Modifying
